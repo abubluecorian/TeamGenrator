b4a5c51e34359047c5cca006d723a6ac0d98075e
abubluecorian <abu786.japit@gmail.com>
2023-10-23 23:18:25 +0530

    teamGenerator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamApp';
+ newMemberName = "";
+ members : string [] = [];
+ onInput(member :string){
+  this.newMemberName = member;
+
+ }
+ addMember(){
+  this.members.push(this.newMemberName);
+  console.log(this.members);
+ }
 }
